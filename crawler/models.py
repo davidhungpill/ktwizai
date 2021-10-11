ed433e50eae9e5ac40da97573265c0a49a41b20a
ultimateneo <aceneo777@egamil.com>
2021-10-11 21:57:24 +0900

    'ee'

--- crawler/models.py
+++ crawler/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class KBOSchedule(models.Model):
-    game_date = models.DateTimeField(auto_now_add=False)
-    away_team = models.CharField(max_length = 100)
-    away_team_score = models.IntegerField(max_length = 10)
-    home_team = models.CharField(max_length = 100)
-    home_team_score = models.IntegerField(max_length = 10)
-    stadium = models.CharField(max_length = 100)
-    result = models.CharField(max_length = 2)
-    note = models.CharField(max_length = 1000)
+# class KBOSchedule(models.Model):
+#     game_date = models.DateTimeField(auto_now_add=False)
+#     away_team = models.CharField(max_length = 100)
+#     away_team_score = models.IntegerField(max_length = 10)
+#     home_team = models.CharField(max_length = 100)
+#     home_team_score = models.IntegerField(max_length = 10)
+#     stadium = models.CharField(max_length = 100)
+#     result = models.CharField(max_length = 2)
+#     note = models.CharField(max_length = 1000)
